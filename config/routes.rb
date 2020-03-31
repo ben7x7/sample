74b45343c993513f8610d8a13a23ad3c64b5ab5c
benoit bessiere <benoit.bessiere@gmail.com>
2020-03-31 18:22:15 +0200

    add portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /en|fr/ do
     root to: 'pages#home'
+    get 'about', to: 'pages#about', as: :about
+    get 'portfolio', to: 'pages#portfolio', as: :portfolio
+    get 'contact', to: 'pages#contact', as: :contact
   end
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
